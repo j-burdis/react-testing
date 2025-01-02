519f981cbdb9fe20d20dcc43e2d0e2e540cc335a
Jonathan Burdis <jonnyburdis@hotmail.com>
2025-01-02 19:37:07 +0000

    write tests using Jest

---
+++ src/helpers/string.js
@@ -0,0 +1,10 @@
+export function capitalize(word) {
+  if (word === null) {
+    return null;
+  }
+  if (word === "") {
+    return "";
+  }
+  return word[0].toUpperCase() +
+    word.substr(1).toLowerCase();
+}
